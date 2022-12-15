22c07868e5132319a3e724e45c4aca6da5c63a89
stevenpease <stevenpease@fb.com>
2022-12-14 18:53:48 -0800

    Apply NVIDIA profiler patch
    Differential Revision: D41483497
    
    fbshipit-source-id: 1c35079ec07f4c3f0f081293dd0a1745159eab70

--- src/misc/profiler.cc
+++ src/misc/profiler.cc
@@ -6,110 +6,119 @@
 
 #include "profiler.h"
 
-//#define PROFILE_PROXY 1
-#ifdef PROFILE_PROXY
-#include "timer.h"
-#include "alloc.h"
+#include <unistd.h>
+#include <sys/time.h>
 
-static const char* profilingStateSendStr[] = { "BufferWait", "GPUWait", "SendWait", "", "End" };
-static const char* profilingStateRecvStr[] = { "BufferWait", "RecvWait", "FlushWait", "GPUWait", "End" };
-static const char* profilingEventStr[] = { "SendRecv", "Sleep", "Idle", "Append" };
-struct ncclProxyProfileEvent {
-  double timestamp[6];
-  uint64_t opCount;
-  int peer;
-  int step;
-  uint16_t channel;
-  uint8_t type; // send / recv
-  uint8_t opIndex;
-};
+double ncclProfilerFreq = -1;
+uint64_t ncclProfilerClockStart;
+void ncclProfilerClockCalibrate() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  uint64_t timeCycles = __rdtsc();
+  double time = - tv.tv_sec*1E6 - tv.tv_usec;
+  uint64_t total = 0ULL;
+  for (int i=0; i<10000; i++) total += __rdtsc();
+  gettimeofday(&tv, NULL);
+  timeCycles = __rdtsc() - timeCycles;
+  time += tv.tv_sec*1E6 + tv.tv_usec;
+  ncclProfilerFreq = timeCycles/time;
+  ncclProfilerClockStart = __rdtsc();
+}
 
-struct ncclProxyProfileEvent* profilingEvents = NULL;
-int profilingIndex = 0;
-double profilingStart = 0;
-#define MAX_EVENTS 200000
+ncclProf_v1_t* ncclProfiler = NULL;
+int ncclProfilerActive = 0;
+struct ncclProfRecord* ncclProfilerRecord = NULL;
+int ncclProfilerNrecs = 0;
+#define NCCL_MAX_PROF_RECS 8
+struct ncclProfRecord* ncclProfilerRecs[NCCL_MAX_PROF_RECS];
 
-ncclResult_t ncclProfilingRecord(struct ncclProxyArgs* args, int sub, int step, int state) {
-  if (profilingEvents == NULL) {
-    NCCLCHECK(ncclCalloc(&profilingEvents, MAX_EVENTS));
-    profilingStart = gettime();
-  }
-  struct ncclProxyProfileEvent* event = NULL;
-  if (state%8 == 0) {
-    if (profilingIndex == MAX_EVENTS) return ncclSuccess;
-    args->subs[sub].profilingEvents[step%NCCL_STEPS] = event = profilingEvents+profilingIndex++;
-    if (state == ncclProxyProfileBegin) {
-      // Proxy operation information
-      event->opCount = args->opCount;
-      event->channel = args->subs[sub].channelId;
-      event->peer = args->subs[sub].peer;
-      event->type = args->pattern;
-      event->step = step;
-      event->opIndex = (((uint64_t)args)/sizeof(struct ncclProxyArgs))%256;
-    } else event->peer = -state;
-  } else {
-    event = (struct ncclProxyProfileEvent*)args->subs[sub].profilingEvents[step%NCCL_STEPS];
-    if (state == ncclProxyProfileEnd) args->subs[sub].profilingEvents[step%NCCL_STEPS] = NULL;
-    if (state == ncclProxyProfileAppendEnd) event->opCount = args->opCount;
-  }
-  // Timestamp
-  event->timestamp[state%8] = gettime()-profilingStart;
+ncclResult_t ncclProfAddRec(struct ncclProfRecord** rec) {
+  NCCLCHECK(ncclCalloc(rec, 1));
+  ncclProfilerRecs[ncclProfilerNrecs++] = *rec;
   return ncclSuccess;
 }
 
-void ncclProfilingDump() {
-  static int dumpDone = 0;
-  if (dumpDone) return;
-  dumpDone = 1;
+ncclResult_t ncclProfGetRecs(int* nRecs, struct ncclProfRecord*** recs) {
+  *nRecs = ncclProfilerNrecs;
+  *recs = ncclProfilerRecs;
+  return ncclSuccess;
+}
+
+ncclResult_t ncclInternalProfilerDumpRecs(int nrecs, struct ncclProfRecord** recs) {
+  if (nrecs == 0) return ncclSuccess;
+
   const char* str = getenv("NCCL_PROXY_PROFILE");
-  if (!str) { free(profilingEvents); return; }
+  if (!str) return ncclSuccess;
   FILE* f = fopen(str, "w");
   fprintf(f, "[\n");
 
-  for (int i=0; i<profilingIndex; i++) {
-    struct ncclProxyProfileEvent* e = profilingEvents+i;
-    const int sendrecv = e->peer >= 0;
-    const char* typeStr = sendrecv ? (e->type == ncclPatternSend ? "Send" : "Recv") :
-      profilingEventStr[-(e->peer/8)];
-
-
-    if (sendrecv) {
-      int state = ncclProxyProfileBegin;
-      const char** stateStr = e->type == ncclPatternSend ? profilingStateSendStr : profilingStateRecvStr;
-      fprintf(f, "{\"name\": \"%s-%d-%d\", \"cat\": \"NET\", \"ph\": \"b\", \"id\": %d, \"pid\": %d, \"tid\": 1, \"ts\": %f, \"args\": { \"opCount\": %ld, \"proxyOpIndex\":%d } },\n",
-          typeStr, e->peer, e->step, i, e->channel, e->timestamp[state], e->opCount, e->opIndex);
-
-      while (state<ncclProxyProfileEnd) {
-        if (e->timestamp[state]) {
-          const char* name = stateStr[state];
-          fprintf(f, "{\"name\": \"%s\", \"cat\": \"NET\", \"ph\": \"b\", \"id\": %d, \"pid\": %d, \"tid\": 1, \"ts\": %f },\n",
-              name, i, e->channel, e->timestamp[state]);
-          state++;
-          while (e->timestamp[state] == 0) state++;
-          fprintf(f, "{\"name\": \"%s\", \"cat\": \"NET\", \"ph\": \"e\", \"id\": %d, \"pid\": %d, \"tid\": 1, \"ts\": %f },\n",
-              name, i, e->channel, e->timestamp[state]);
+  for (int r=0; r<nrecs; r++) {
+    struct ncclProfRecord* rec = recs[r];
+    for (int i=0; i<rec->nextId; i++) {
+      struct ncclProfEvent* e = rec->events[ID_BLOCK(i)]+ID_IDX(i);
+      fprintf(f, "{ \"name\": \"%s\", \"cat\": \"NET\", \"pid\": %d, \"tid\": %d, \"ts\": %f, \"id\": %d, \"ph\": \"b\", \"args\": { ", rec->typeNames[e->type], rec->rank, rec->rank, e->ts[NCCL_PROF_TS_START], i);
+      for (int i=0; i<rec->kvSize; i++) {
+        if (i) fprintf(f, ", ");
+        fprintf(f, "\"%s\": ", rec->kvNames[i]);
+        switch (rec->kvPrintFmt[i]) {
+          case 'd': fprintf(f, "%ld", e->kv[i]); break;
+          case 'u': fprintf(f, "%lu", e->kv[i]); break;
+          case 'x': fprintf(f, "0x%lx", e->kv[i]); break;
+          default:
+                    printf("Unknown format %d\n", rec->kvPrintFmt[i]);
+                    return ncclInternalError;
         }
       }
-
-      fprintf(f, "{\"name\": \"%s-%d-%d\", \"cat\": \"NET\", \"ph\": \"e\", \"id\": %d, \"pid\": %d, \"tid\": 1, \"ts\": %f },\n",
-          typeStr, e->peer, e->step, i, e->channel, e->timestamp[state]);
-    } else {
-      if (e->peer == -ncclProxyProfileAppend) {
-      fprintf(f, "{\"name\": \"%s\", \"cat\": \"NET\", \"ph\": \"b\", \"id\": %d, \"pid\": -1, \"tid\": 1, \"ts\": %f, \"args\": { \"added\": %ld } },\n",
-          typeStr, i, e->timestamp[0], e->opCount);
-      } else {
-        fprintf(f, "{\"name\": \"%s\", \"cat\": \"NET\", \"ph\": \"b\", \"id\": %d, \"pid\": -1, \"tid\": 1, \"ts\": %f },\n",
-          typeStr, i, e->timestamp[0]);
+      fprintf(f, " } },\n");
+      int startIndex = NCCL_PROF_TS_START;
+      for (int tsIndex=2; tsIndex<NCCL_PROF_TS_SIZE; tsIndex++) {
+        if (e->ts[tsIndex] == e->ts[NCCL_PROF_TS_START]) continue;
+        fprintf(f, "{ \"name\": \"%s\", \"cat\": \"NET\", \"pid\": %d, \"tid\": %d, \"id\": %d, \"ts\": %f, \"ph\": \"%c\" },\n", rec->tsNames[tsIndex], rec->rank, rec->rank, i, e->ts[startIndex], 'b');
+        fprintf(f, "{ \"name\": \"%s\", \"cat\": \"NET\", \"pid\": %d, \"tid\": %d, \"id\": %d, \"ts\": %f, \"ph\": \"%c\" },\n", rec->tsNames[tsIndex], rec->rank, rec->rank, i, e->ts[tsIndex], 'e');
+        startIndex = tsIndex;
       }
-      fprintf(f, "{\"name\": \"%s\", \"cat\": \"NET\", \"ph\": \"e\", \"id\": %d, \"pid\": -1, \"tid\": 1, \"ts\": %f },\n",
-          typeStr, i, e->timestamp[1]);
+      fprintf(f, "{ \"name\": \"%s\", \"cat\": \"NET\", \"pid\": %d, \"tid\": %d, \"ts\": %f, \"id\": %d, \"ph\": \"e\" },\n", rec->typeNames[e->type], rec->rank, rec->rank, e->ts[NCCL_PROF_TS_END], i);
     }
   }
+
   fprintf(f, "{} ]\n");
   fclose(f);
-  free(profilingEvents);
+  return ncclSuccess;
 }
-#else
-ncclResult_t ncclProfilingRecord(struct ncclProxyArgs* args, int sub, int step, int state) { return ncclSuccess; }
-void ncclProfilingDump() {}
-#endif
+
+ncclProfGetRecFn_t ncclInternalProfilerGetRec;
+ncclResult_t ncclInternalProfilerInit(int* active, ncclProfGetRecFn_t getRec) {
+  ncclInternalProfilerGetRec = getRec;
+  if (atoi(getenv("NCCL_PROFILE")) == 1) *active = 1;
+  return ncclSuccess;
+}
+
+ncclResult_t ncclInternalProfilerExit() {
+  int nrecs;
+  struct ncclProfRecord** recs;
+  NCCLCHECK(ncclInternalProfilerGetRec(&nrecs, &recs));
+  NCCLCHECK(ncclInternalProfilerDumpRecs(nrecs, recs));
+  return ncclSuccess;
+}
+
+ncclProf_v1_t ncclInternalProfiler = {
+  "Internal",
+  ncclInternalProfilerInit,
+  ncclInternalProfilerExit
+};
+
+ncclResult_t ncclProfInit() {
+  void* profPluginLib = dlopen("libnccl-prof.so", RTLD_NOW | RTLD_LOCAL);
+  if (profPluginLib) {
+    ncclProfiler = (ncclProf_v1_t*)dlsym(profPluginLib, "ncclProfPlugin_v1");
+  } else ncclProfiler = &ncclInternalProfiler;
+  ncclProfiler->init(&ncclProfilerActive, ncclProfGetRecs);
+  ncclProfilerClockCalibrate();
+  return ncclSuccess;
+}
+
+ncclResult_t ncclProfStop() {
+  ncclProfiler->exit();
+  for (int r=0; r<ncclProfilerNrecs; r++) ncclProfFreeRec(ncclProfilerRecs[r]);
+  return ncclSuccess;
+}

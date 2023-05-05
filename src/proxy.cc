9b910b531b723aee3a7217f6b4d857b1411db1f4
Min Si <msi@meta.com>
2023-05-05 14:24:48 -0700

    dump profiling results when destroy last comm
    Summary:
    The default behavior is to dump tracing/profiling events whenevern destory a communicator which internally outputs the events into a file. The later dumps will generate empty trace files and overwrite the first trace file which contains all events.
    
    This patch changes the behavior to dump only when destroying the last communicator, thus avoiding such overwriting behavior.
    
    Reviewed By: wesbland
    
    Differential Revision: D45033242
    
    fbshipit-source-id: 723a6b0721bd2f765a1039212195be739df013d8

--- src/proxy.cc
+++ src/proxy.cc
@@ -898,6 +898,7 @@ static ncclResult_t ncclProxyProgressCreate(struct ncclProxyState* proxyState) {
   return ncclSuccess;
 }
 
+static int nProxyComms = 0;
 ncclResult_t ncclProxyProgressDestroy(struct ncclProxyState* proxyState) {
   struct ncclProxyProgressState* state = &proxyState->progressState;
 
@@ -916,9 +917,12 @@ ncclResult_t ncclProxyProgressDestroy(struct ncclProxyState* proxyState) {
     free(state->pools);
     state->pools = next;
   }
-
-  ncclProfilingDump();
-  ntraceProfilingDump();
+  // Dump profiling results only after destroying the last communicator
+  nProxyComms--;
+  if (nProxyComms == 0){
+    ncclProfilingDump();
+    ntraceProfilingDump();
+  }
   TIME_PRINT("Proxy");
   return ncclSuccess;
 }
@@ -1390,6 +1394,8 @@ static bool proxyMatchOpType(int type) {
 
 void* ncclProxyService(void* _args) {
   struct ncclProxyState* proxyState =  (struct ncclProxyState*) _args;
+  printf("pid %d ncclProxyService for proxyState %p, tpRank %d tpnRanks %d, nProxyComms=%d\n", getpid(), proxyState, proxyState->tpRank, proxyState->tpnRanks, nProxyComms);
+  nProxyComms++;
   // if (CPU_COUNT(&comm->cpuAffinity)) sched_setaffinity(0, sizeof(cpu_set_t), &comm->cpuAffinity);
   if (setProxyThreadContext(proxyState)) {
     INFO(NCCL_INIT, "[Proxy Service] Created CUDA context on device %d", proxyState->cudaDev);
